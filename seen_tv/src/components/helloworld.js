1bd334c5a66dc8d24a80793037ad38b61cca0961
Joshua Land <joshua.land6@gmail.com>
2023-02-06 18:26:57 -0800

    Hello world up and running

---
+++ seen_tv/src/components/helloworld.js
@@ -0,0 +1,17 @@
+
+function HelloWorld(){
+    return (
+    <html>
+        <head>
+         <title>
+            Hello World
+         </title>
+        </head>
+        <body>
+            <h1>Hello World!!!</h1>
+        </body>
+    </html>
+    );
+}
+
+export default HelloWorld;
